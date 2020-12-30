bb41d18401a6f27ae192dc5236255451c811d7bf
Kyle Falconer <kfalconer@gmail.com>
2020-12-29 20:20:53 -0800

    formatted name constants

--- name_constants.py
+++ name_constants.py
@@ -2,19 +2,48 @@ from Name import Region
 
 religious_indicators = ["lord", "god", "g-d", "goddess", "jesus", "zeus", "virgin", "brahma", "vishnu", "shiva",
                         "ganapati", "lakshmi", "durga", "krishna", "hanuman", "agni", "surya", "indra", "religious",
-                        "king david", "old testament", "new testament"]
+                        "king david", "old testament", "new testament", "baptist", "saint"]
 excluded_name_endings = (
-'ita', 'line', 'ina', 'loena', 'iden', 'aden', 'adun', 'idun', 'idena', 'adena', 'aduna', 'iduna')
+    'ita', 'line', 'ina', 'loena', 'iden', 'aden', 'adun', 'idun', 'idena', 'adena', 'aduna', 'iduna')
 excluded_startings = ('a', 'k', 'jy', 'eu', 'mary', 'mara', 'bah', 'baka', 'brit', 'shawn', 'hild')
 excluded_contains = ('kini', 'ika', 'eswit', 'sheba', 'shit', 'sheet')
 hard_to_pronounce = ('kh', 'gh', 'haa', 'ayn', 'aij')
 
-western_origins = [Region.United_States, Region.United_Kingdom, Region.English, Region.Germany, Region.France,
-                   Region.Italy]
-indian_origins = [Region.Tamil, Region.Malayalam, Region.Sanskrit, Region.Telugu, Region.Muslim, Region.Bengali,
-                  Region.Marathi, Region.Punjabi]
+western_origins = [
+    Region.English,
+    Region.France,
+    Region.Germany,
+    Region.Italy,
+    Region.United_Kingdom,
+    Region.United_States
+]
+
+indian_origins = [
+    Region.Bengali,
+    Region.Malayalam,
+    Region.Marathi,
+    Region.Muslim,
+    Region.Punjabi,
+    Region.Sanskrit,
+    Region.Tamil,
+    Region.Telugu
+]
 
 # exclude names that have a single origin in any of these countries or regions
-exclude_only_origins = [Region.Japan, Region.Saudi_Arabia, Region.South_Africa, Region.Ireland, Region.African_American,
-                        Region.Africa, Region.Afghanistan, Region.China, Region.Latin_America, Region.Spain,
-                        Region.Mexico, Region.Scotland, Region.South_Korea, Region.North_Korea, Region.Taiwan]
+exclude_only_origins = [
+    Region.Afghanistan,
+    Region.Africa,
+    Region.African_American,
+    Region.China,
+    Region.Ireland,
+    Region.Japan,
+    Region.Latin_America,
+    Region.Mexico,
+    Region.North_Korea,
+    Region.Saudi_Arabia,
+    Region.Scotland,
+    Region.South_Africa,
+    Region.South_Korea,
+    Region.Spain,
+    Region.Taiwan
+]
